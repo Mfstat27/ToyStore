33b91f985e551279977dd9a56dd13a093c510c1e
Tracey Thomas <tracey_thomas@live.com>
2021-08-31 05:47:15 -0400

    removing problems causing trouble with deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import indexCss from './index.css';
 import App from './App';
 import {ProductProvider} from './context';
 import reportWebVitals from './reportWebVitals';
